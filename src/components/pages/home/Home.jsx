ca68bcf79fc60247fa00d79400677aff1c05ad2f
FerbaDev <elferbarron1@gmail.com>
2023-07-30 19:34:18 -0300

    se realiza peticion con axios

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,3 +1,13 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export const Home = () => {
+  useEffect(() => {
+    axios
+      .get(" http://localhost:5000/movies")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return <div>Home</div>;
 };
