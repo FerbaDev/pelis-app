614f2337577e6375cbe2c49ae7c0b8eec505149c
FerbaDev <elferbarron1@gmail.com>
2023-08-04 01:25:17 -0300

    se crea componente CreateMovie

--- src/components/common/cardMovie/CardMovie.jsx
+++ src/components/common/cardMovie/CardMovie.jsx
@@ -1,38 +1,15 @@
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-
 import FavoriteIcon from "@mui/icons-material/Favorite";
-
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
-import { useState } from "react";
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
 
 export const CardMovie = ({ movie, handleLike }) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
   return (
     <Card sx={{ maxWidth: 345, height: 500 }}>
       <CardHeader title={movie.name} subheader={movie.createdAt} />
@@ -52,20 +29,7 @@ export const CardMovie = ({ movie, handleLike }) => {
             onClick={() => handleLike(movie)}
           />
         </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{movie.extra}</Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 };
